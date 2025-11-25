a285fdc09950e36005b70ed4b680e5210d68268c
utwoa <loskon64@gmail.com>
2025-11-25 18:40:51 +0300

    upd

--- bot.py
+++ bot.py
@@ -5,29 +5,22 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 BOT_TOKEN = "8586920536:AAHmc9iFU073Zvj-Ebt9G9OtSut9FsWxB0c"
 bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
 
-async def send_user_message(chat_id: int, product_name: str, start_parameter: str):
-
-    # Сырая кнопка Mini App
-    raw_keyboard = {
-        "inline_keyboard": [
+async def send_user_message(chat_id: int):
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
             [
-                {
-                    "text": "Открыть приложение",
-                    "request_main_web_view": {
-                        "bot_username": "more_stars_bot",
-                        "start_parameter": start_parameter,
-                        "mode": "fullscreen"
+                InlineKeyboardButton(
+                    text="Открыть мини-приложение",
+                    request_main_web_view={
+                        "url": "https://t.me/more_stars_bot?startapp&mode=compact"
                     }
-                }
+                )
             ]
         ]
-    }
-
-    # Создаём клавиатуру БЕЗ валидации aiogram
-    keyboard = InlineKeyboardMarkup.model_construct(**raw_keyboard)
+    )
 
     await bot.send_message(
-        chat_id=chat_id,
-        text=f"🎉 Оплата за <b>{product_name}</b> прошла успешно!",
+        chat_id,
+        "Нажми кнопку для открытия мини-приложения:",
         reply_markup=keyboard
     )
